34733ef710a5f062ae74bea4dc6bb7c648e68454
Richard Tatum <rtatum@pm.me>
2024-07-09 16:27:32 +0100

    Add a cleanup for any removed hosts whilst the bot is running

--- Bot/Features/StreamChecker/PeriodicStreamChecker.cs
+++ Bot/Features/StreamChecker/PeriodicStreamChecker.cs
@@ -23,7 +23,8 @@ public class PeriodicStreamChecker : BackgroundService
     {
         using var timer = new PeriodicTimer(TimeSpan.FromSeconds(60));
 
-        var subscribedHosts = new Dictionary<int, CancellationTokenSource>();
+        // TODO: Consider the fact that removed hosts could be replaced by a different host with the same Id, causing problems with running subscriptions
+        var subscribedHosts = new Dictionary<int, (CancellationTokenSource cancellationTokenSource, string url)>();
         while (await timer.WaitForNextTickAsync(stoppingToken))
         {
             // TODO: Check for hosts that are subscribed, but no longer in the DB to clear them
@@ -40,10 +41,13 @@ public class PeriodicStreamChecker : BackgroundService
                     "[HOST-CHECKER] New host found: {Host}. Creating subscription. Interval: {Seconds}s", host.Url,
                     host.PollIntervalSeconds);
 
+                // Create a child of the provided cancellation token
                 var cancellationSource = CancellationTokenSource.CreateLinkedTokenSource(stoppingToken);
-                subscribedHosts[host.Id] = cancellationSource;
+                subscribedHosts[host.Id] = (cancellationSource, host.Url);
                 _ = SubscribeToHostAsync(TimeSpan.FromSeconds(host.PollIntervalSeconds), host, cancellationSource.Token);
             }
+
+            await CleanupStaleSubscriptionsAsync(subscribedHosts, hosts);
         }
     }
 
@@ -58,14 +62,40 @@ public class PeriodicStreamChecker : BackgroundService
             var subscriptions = await _query.GetSubscriptionsAsync(host.Id);
             if (subscriptions.Length == 0)
             {
-                _logger.LogInformation("[KEY-CHECKER][{HOST}] No key subscriptions found. Skipping.", host.Url);
+                _logger.LogInformation("[KEY-CHECKER][{Host}] No key subscriptions found. Skipping.", host.Url);
                 return;
             }
 
             _logger.LogInformation(
-                "[KEY-CHECKER][{HOST}] {Subscriptions} key subscription(s) found. Checking for stream updates.",
+                "[KEY-CHECKER][{Host}] {Subscriptions} key subscription(s) found. Checking for stream updates.",
                 host.Url, subscriptions.Length);
             await _service.CheckStreamsAsync(host, subscriptions);
         }
+    }
+
+    private async Task CleanupStaleSubscriptionsAsync(
+        Dictionary<int, (CancellationTokenSource cancellationTokenSource, string url)> subscribedHosts, 
+        IEnumerable<Host> databaseHosts)
+    {
+        // These are hosts that were previously subscribed but have since been removed from the DB
+        var removedHosts = subscribedHosts
+            .Where(subscription => 
+                !databaseHosts.Select(host => host.Id).Contains(subscription.Key)
+            )
+            .ToArray();
+
+        var cancellationTasks = removedHosts.Select(entry =>
+        {
+            var (cancellationSource, url) = entry.Value;
+            return CancelSubscriptionAsync(cancellationSource, url);
+        });
+
+        await Task.WhenAll(cancellationTasks);
+    }
+
+    private Task CancelSubscriptionAsync(CancellationTokenSource cancellationSource, string url)
+    {
+        _logger.LogInformation("[HOST-CHECKER][{Host}] Host no longer registered in the database. Cancelling.", url);
+        return cancellationSource.CancelAsync();
     }
 }
