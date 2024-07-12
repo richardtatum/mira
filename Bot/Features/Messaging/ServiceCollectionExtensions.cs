a9696fb5600a512669a9f35d9ae16a605a5d5685
Richard Tatum <rtatum@pm.me>
2024-07-12 15:30:30 +0100

    Remove the messaging service query repository

--- Bot/Features/Messaging/ServiceCollectionExtensions.cs
+++ Bot/Features/Messaging/ServiceCollectionExtensions.cs
@@ -1,5 +1,4 @@
 using Microsoft.Extensions.DependencyInjection;
-using Mira.Features.Messaging.Repositories;
 
 namespace Mira.Features.Messaging;
 
@@ -8,6 +7,5 @@ public static class ServiceCollectionExtensions
     public static void AddMessagingService(this IServiceCollection services)
     {
         services.AddScoped<IMessageService, MessageService>();
-        services.AddScoped<QueryRepository>();
     }
 }
