8562f74332b30ad0450dab86e7fef08064a0cb21
Richard Tatum <rtatum@pm.me>
2025-07-27 12:55:46 +0100

    Add auth header value to host model. Move to shared project

--- src/Mira/Modules/Commands/Commands.Core/AddHost/Models/Host.cs
+++ src/Mira/Modules/Commands/Commands.Core/AddHost/Models/Host.cs
@@ -1,3 +1,3 @@
 namespace Commands.Core.AddHost.Models;
 
-public record Host(string Url, int PollIntervalSeconds, ulong GuildId, ulong CreatedBy);
+public record Host(string Url, int PollIntervalSeconds, ulong GuildId, ulong CreatedBy, string? AuthHeader = null);
